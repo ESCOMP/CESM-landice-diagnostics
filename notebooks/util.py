8ef3ba7ef524bf7ef0a761354def51478ec1a0b6
Kate Thayer-Calder <katec@ucar.edu>
2021-04-02 10:12:16 -0600

    Initial commit

---
+++ notebooks/util.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def some_calc(ds):
+    """
+    Insert docstring here
+    """
+
+    # Do some sort of calculation
+    out = ds.some_variable * equation
+    return out
+# -----------------------------------
+# REMOVE ABOVE THIS LINE
